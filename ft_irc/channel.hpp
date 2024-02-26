d6952213c5a8524b899d61b1109ed99fbc3f4beb
fhihi <fouadhihi9@gmail.com>
2024-02-26 15:42:26 +0100

    trying to push to main

--- ft_irc/channel.hpp
+++ ft_irc/channel.hpp
@@ -5,13 +5,43 @@
 #include<list>
 
 class	client;
+class	server;
+
+
+#define ERR_NEEDMOREPARAMS(nick, command)	("461 " nick + " " + command + " :Not enough parametes\r\n")
+#define ERR_ALREADYREGISTERED(nick)			("462 " nick + " :You may not reregister\r\n")
+#define ERR_PASSWDMISMATCH(nick)			("464 " nick + " :Password incorrect\r\n")
+#define ERR_CHANNELISFULL(nick, chan)		("471 " nick + " " + chan + " :Cannot join channel (+l)\r\n")
+#define ERR_UNKNOWNMODE(nick, mode)			("472 " nick + " " + mode + " :is unknown mode char to me\r\n")
+#define ERR_INVITEONLYCHAN(nick, chan)		("473 " nick + " " + chan + " :Cannot join channel (+i)\r\n")
+#define ERR_BANEDFROMCHAN(nick, chan)		("473 " nick + " " + chan + " :Cannot join channel (+b)\r\n")
+#define ERR_BADCHANNELKEY(nick, chan)		("475 " nick + " " + chan + " :cannot join channel (+k)\r\n")
+#define ERR_BADCHANMASK(chan)				("476 "  chan + " :Bad Channel Mask\r\n")
+#define ERR_USERONCHANNEL(nick )
+#define ERR_
+#define ERR_
+#define ERR_
+#define ERR_
+
 
 class	channel {
 private:
+	bool				_inviteOnly;
+
 	std::string			_name;
+	std::string			_creator;
+
+	bool				_hasPass;
 	std::string			_passwd;
+
+	bool				_hasTopic;
+	std::string			_topicSetter;
 	std::string			_topic;
 
+	time_t				_creationTime;
+
+	int					_maxUsers;
+	// client				_server;
 public:
 
 	std::list<client*>	_members;
